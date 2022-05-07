ec1dc951d8cfbec6029f0d71160844035c42af53
lerwen <100460400+lerwen@users.noreply.github.com>
2022-05-07 12:14:24 +0800

    Update [條件判斷] 學生證掉了怎麼辦?.cpp

--- 116/[條件判斷] 學生證掉了怎麼辦?.cpp
+++ 116/[條件判斷] 學生證掉了怎麼辦?.cpp
@@ -25,30 +25,39 @@ using pii = std::pair<int, int>;
 
 void solve()
 {
-    /*
+   /*
         不知道題序是什麼，目前使用原code，之後修改
+        類似身份證 共九碼
+        第一碼為 城市編號 21-40 間
+        第二碼為 性別 0-2 間
+        其餘7碼 計算用  
     */
-    int a,b,c,d,e,f,g,h,i,fifi,fise,t1,t2,t3,t4,t5;
-	cin>>a>>b>>c>>d>>e>>f>>g>>h>>i;
-	if((a<20||a>41)&&(b<0||b>2)){
-		cout<<"Terrible Fake\n";
+    int a,b,input,t1,t2,t3,t4,t5,sum=0;
+	cin >> a >> b;
+    	for(int i=2;i<=8;i++){
+        	cin >> input;
+        	sum += input * i;  
+    	}
+
+	if( ( a<20 || a>41 ) && ( b<0 || b>2 ) ){
+		cout << "Terrible Fake\n";
 	}
-	else if(a<20||a>41){
-		cout<<"Fake city\n";
+	else if( a<20 || a>41 ){
+		cout << "Fake city\n";
 	}
-	else if(b<0||b>2){
-		cout<<"The fourth gender\n";
+	else if( b<0 || b>2 ){
+		cout << "The fourth gender\n";
 	}
 	else{
-		fifi=a%10;
-		fise=a/10;
-		t1=fifi*2+fise*3;
-		t2=1*b+2*c+3*d+4*e+5*f+6*g+7*h+8*i;
-		t3=t1+t2;
-		t4=t3%10;
-		t5=10-t4;
-		if(t4==0) cout<<"0\n";
-		else cout<<t5<<endl;
+		t1= ( a % 10 )*2 + ( a / 10 ) * 3;
+		t2= b + sum;
+		t4= ( t1 + t2 ) % 10;
+		t5= 10 - t4;
+		
+		if( t4 == 0 ) 
+            	    cout << "0\n";
+		else 
+            	    cout << t5 << '\n';
 	}
 }
 
