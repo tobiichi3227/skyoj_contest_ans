f0e85c889aea16f03a6ce94afcfa7f4e64811b56
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-05-09 10:58:06 +0800

    complete 25.py

--- 116/test25.py
+++ 116/test25.py
@@ -1,29 +1,8 @@
-#include <bits/stdc++.h>
-using namespace std;
+def miyuki_is_my_wife():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    print(arr[n - 2], '\n')
 
-#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
-
-int main()
-{
-    miyuki_is_my_wife;
-
-    /*
-        應該是在arr中找第二大的，變數名使用原code
-    */
-    int n{}, b1{}, b2{}, val{};
-    cin >> n;
-    while (n--) {
-        cin >> val;
-        if (val > b1) {
-            b2 = b1;
-            b1 = val;
-        } else if (val > b2) {
-            b2 = val;
-        }
-    }
-
-    cout << b2 << '\n';
-
-    return 0;
-}
-
+if __name__ == "__main__":
+    miyuki_is_my_wife()
